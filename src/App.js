0581ad1f75bd66719057dea4c83fc553440b6e2c
Anshika Verma <anshikaverma2411@gmail.com>
2024-09-10 17:04:43 +0530

    Build accordion using react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "./components/Accordion_component";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
