94243280eac9ad3277b3758802209150291222cc
Anshika Verma <anshikaverma2411@gmail.com>
2024-09-12 14:53:33 +0530

    Build image-slider using react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Accordion from "./components/Accordion_component";
 import RandomColor from "./components/random-color-genetator";
 import StarRating from "./components/star-rating";
+import ImageSlider from "./components/Image-Slider";
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* Star-Rating component */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* Image Slider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
